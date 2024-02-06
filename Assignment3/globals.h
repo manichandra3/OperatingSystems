4842ab701d7d850cee893218572ba7ccd017f6e5
Mani Chandra <127130821+manichandra3@users.noreply.github.com>
2024-02-06 21:44:44 +0530

    added a new header for global vars and other readability enhancements

---
+++ Assignment3/globals.h
@@ -0,0 +1,13 @@
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#define MAX_PROCESS 20
+typedef struct {
+  char *name;
+  int arrival;
+  int cpuburst;
+  int turnaround;
+  int wait;
+} Process;
+Process processtable[MAX_PROCESS];
+#endif // GLOBALS_H
