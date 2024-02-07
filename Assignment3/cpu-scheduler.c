70d4a4c1bbfd5427c69de6d1a5f1083a04a23e49
Mani Chandra <127130821+manichandra3@users.noreply.github.com>
2024-02-07 17:30:30 +0530

    Added 'header.h' for scheduling algorithms and 'globals.h' for handling global variables

--- Assignment3/cpu-scheduler.c
+++ Assignment3/cpu-scheduler.c
@@ -1,57 +1,17 @@
+#include "globals.h"
 #include "header.h"
 #include <fcntl.h>
-#include <malloc/_malloc_type.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_PROCESS 4
-// #define PROCESS_COUNT 0
-
+// Globals
 int PROCESS_COUNT = 0;
+extern Process processtable[MAX_PROCESS];
 
-typedef struct {
-  char *name;
-  int arrival;
-  int cpuburst;
-  int turnaround;
-  int wait;
-} Process;
-Process processtable[MAX_PROCESS];
-// typedef struct{
-//     Process **process;
-//     int count;
-// }ProcessTable ;
-
-Process *ReadProcessTable(char *filename) {
-  FILE *file = fopen(filename, "r");
-  char *buf[100];
-  if (open(filename, O_RDONLY) < 0) {
-    fprintf(stderr, "error while opening %s.txt", filename);
-    exit(EXIT_FAILURE);
-  } else {
-    char input[25];
-    while (fgets(input, 25, file)) {
-      char *p_name = malloc(6);
-      int arrival, cpu_burst;
-      fscanf(file, "%s %d %d\n", p_name, &arrival, &cpu_burst);
-      printf("%s %d %d\n", p_name, arrival, cpu_burst);
-      Process *pr = malloc(sizeof(Process));
-      processtable[PROCESS_COUNT].name = p_name;
-      processtable[PROCESS_COUNT].arrival = arrival;
-      processtable[PROCESS_COUNT].cpuburst = cpu_burst;
-      processtable[PROCESS_COUNT].wait = -1;
-      processtable[PROCESS_COUNT].turnaround = -1;
-      PROCESS_COUNT++;
-    }
-  }
-  return processtable;
-}
-
-void PrintProcessTable() {
-  for (int i = 0; i < PROCESS_COUNT; i++) {
-    printf("%s", processtable[i].name);
-  }
-}
+// Function Prototypes
+void ReadProcessTable(char *filename);
+void PrintProcessTable();
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -62,6 +22,92 @@ int main(int argc, char *argv[]) {
   char *inputfile = argv[1];
   ReadProcessTable(inputfile);
   PrintProcessTable();
-
+  //
+  // Menu Header
+  //
+  char option;
+  printf("---------------------------------------------\n");
+  printf("         Cpu Scheduling Simulation\n");
+  printf("---------------------------------------------\n");
+  printf("Select Scheduling Algorithm [1,2,3 or 4]\n");
+  printf("1.First Come First Serve.\n");
+  printf("2.Round Robin.\n");
+  printf("3.Shortest Burst First.\n");
+  printf("4.Exit.\n");
+  scanf(" %c", &option);
+  while (option != '4') {
+    switch (option) {
+    case '1':
+      // function to perform FCFS.
+      FCFS(processtable, PROCESS_COUNT);
+      break;
+    case '2':
+      // function to perform RR
+      printf("Enter Time Quantum: ");
+      int quantum = 1;
+      scanf("%d", &quantum);
+      RR(processtable, PROCESS_COUNT, quantum);
+      break;
+    case '3':
+      // function to perform SRBF.
+      break;
+    default:
+      printf("Invalid Input!\n");
+    }
+    printf("---------------------------------------------\n");
+    printf("         Cpu Scheduling Simulation\n");
+    printf("---------------------------------------------\n");
+    printf("Select Scheduling Algorithm [1,2,3 or 4]\n");
+    printf("1.First Come First Serve.\n");
+    printf("2.Round Robin.\n");
+    printf("3.Shortest Burst First.\n");
+    printf("4.Exit.\n");
+    scanf(" %c", &option);
+  }
   return 0;
+}
+
+void ReadProcessTable(char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    fprintf(stderr, "Error while opening %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("File Found!\n");
+  printf("Reading from file...\n");
+
+  char p_name[3];
+  int arrival, cpu_burst;
+
+  while (fscanf(file, "%2s %d %d", p_name, &arrival, &cpu_burst) == 3 &&
+         PROCESS_COUNT < MAX_PROCESS) {
+    processtable[PROCESS_COUNT].name = strdup(p_name);
+    if (processtable[PROCESS_COUNT].name == NULL) {
+      fprintf(stderr, "Memory allocation failed\n");
+      exit(EXIT_FAILURE);
+    }
+    // printf("%s %d %d\n", p_name, arrival, cpu_burst);
+    processtable[PROCESS_COUNT].arrival = arrival;
+    processtable[PROCESS_COUNT].cpuburst = cpu_burst;
+    processtable[PROCESS_COUNT].wait = -1;
+    processtable[PROCESS_COUNT].turnaround = -1;
+    // printf("%s %d %d\n", processtable[PROCESS_COUNT].name,
+    //        processtable[PROCESS_COUNT].arrival,
+    //        processtable[PROCESS_COUNT].cpuburst);
+    // printf("PROCESS_COUNT updated to: %d", PROCESS_COUNT);
+    PROCESS_COUNT++;
+  }
+
+  fclose(file);
+}
+
+void PrintProcessTable() {
+  printf("Displaying From File...\n");
+  if (sizeof(processtable) / sizeof(processtable[0]) == 0) {
+    printf("Empty Table!\n");
+  }
+  for (int i = 0; i < PROCESS_COUNT; i++) {
+    printf("%s\n", processtable[i].name);
+  }
 }
--- Assignment3/header.h
+++ Assignment3/header.h
@@ -1,1 +1,154 @@
+// FCFS:
+//  Waiting Time(wt[i]) = at[i – 1] + bt[i – 1] + wt[i – 1] ) – at[i]
+//  Turnaround Time(tu[i]) = wt[i] + bt[i]
+//  Average Waiting Time = (sum of all waiting time)/(Number of processes)
+#include "globals.h"
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// Globals
+unsigned int CURRENT_TIME;
+// A struct defines components process stack.
+typedef struct {
+  int startTime;
+  int processId;
+  int remainingBurst;
+  int endTime;
+} ProcessInfo;
+// process stack
+ProcessInfo processInfoArray[20];
+
+// Function Prototypes
+void FCFS(Process *processtable, int PROCESS_COUNT);
+void RR(Process *processtable, int PROCESS_COUNT, int quantum);
+void SRBF(Process *processtable);
+void PrintStatistics(Process *processtable, float avgWait, float avgTurn,
+                     int count, int flag);
+
+// IMPLEMENTATIONS
+void FCFS(Process *processtable, int PROCESS_COUNT) {
+  // keeps track of sum of wait times
+  float sum = 0;
+  // keeps track of sum of turnaround times
+  float sumT = 0;
+  for (int i = 0; i < PROCESS_COUNT; i++) {
+    if (i == 0) {
+      processInfoArray[i].startTime = CURRENT_TIME;
+      CURRENT_TIME += processtable[i].cpuburst;
+      processtable[i].wait = 0;
+      processtable[i].turnaround += processtable[i].cpuburst;
+      processInfoArray[i].endTime = CURRENT_TIME; // First process ends
+    } else {
+      processtable[i].wait =
+          (processtable[i - 1].wait + processtable[i - 1].arrival +
+           processtable[i - 1].cpuburst) -
+          processtable[i].arrival;
+      processtable[i].turnaround =
+          processtable[i].wait + processtable[i].cpuburst;
+      sum += processtable[i].wait;
+      sumT += processtable[i].turnaround;
+      // the curent process starts when the previous one ends.
+      processInfoArray[i].startTime = processInfoArray[i - 1].endTime;
+      CURRENT_TIME += processtable[i].cpuburst;
+      processInfoArray[i].endTime = CURRENT_TIME;
+    }
+  }
+  float AverageWaitTime = sum / PROCESS_COUNT;
+  float AverageTurnaround = sumT / PROCESS_COUNT;
+  PrintStatistics(processtable, AverageWaitTime, AverageTurnaround,
+                  PROCESS_COUNT, 1);
+}
+
+void RR(Process *processtable, int PROCESS_COUNT, int quantum) {
+  float sum = 0;
+  float sumT = 0;
+
+  unsigned int index = 0;
+  unsigned int complete = 0;
+  // Initiating the processes
+  bool isArrived[PROCESS_COUNT];
+  bool isComplete[] = {0};
+  for (int i = 0; i < PROCESS_COUNT; i++) {
+    if (processtable[i].arrival == 0) {
+      isArrived[i] = 1;
+    }
+    processInfoArray[i].processId = i;
+    processInfoArray[i].remainingBurst = processtable[i].cpuburst;
+  }
+  while (complete < PROCESS_COUNT) {
+    if (processtable[index].arrival <= CURRENT_TIME &&
+        processInfoArray[index].remainingBurst != 0) {
+
+      if (processInfoArray[index].remainingBurst - quantum <= 0 &&
+          isComplete[index] == 0) {
+
+        isComplete[index] = 1;
+        complete++; // completed the process indicated by its index
+        processInfoArray[complete].startTime = CURRENT_TIME;
+        processInfoArray[complete].processId = index;
+        CURRENT_TIME += processInfoArray[index]
+                            .remainingBurst; // update remaining burst time
+        processInfoArray[index].remainingBurst = 0;
+        processInfoArray[complete].endTime =
+            CURRENT_TIME; // this is where the process is finished process
+
+        if (processtable[index].wait == -1) {
+          processtable[index].wait =
+              CURRENT_TIME; // the process default wait never updated => waiting
+                            // all this time.
+        }
+      } else if (processInfoArray[index].remainingBurst - quantum >= 0 &&
+                 isComplete[index] == 0) {
+        processInfoArray[complete].startTime = CURRENT_TIME;
+        CURRENT_TIME += quantum;
+        processInfoArray[complete].processId = index;
+        processInfoArray[index].remainingBurst -= quantum;
+        processInfoArray[complete].endTime = CURRENT_TIME;
+      }
+    }
+    if (index == PROCESS_COUNT - 1) {
+      index = 0;
+    } else {
+      index += 1;
+    }
+  }
+  for (int i = 0; i < PROCESS_COUNT; i++) {
+    printf("wait times: %d\n", processtable[i].wait);
+  }
+}
+
+void SRBF(Process *processtable) {}
+
+void PrintStatistics(Process *processtable, float avgWait, float avgTurn,
+                     int count, int flag) {
+  if (flag == 1) {
+    printf("---------------------------------------------\n");
+    printf("                   FCFS\n");
+    printf("---------------------------------------------\n");
+  } else if (flag == 2) {
+    printf("---------------------------------------------\n");
+    printf("                    RR\n");
+    printf("---------------------------------------------\n");
+  } else if (flag == 3) {
+    printf("---------------------------------------------\n");
+    printf("                   SRBF\n");
+    printf("---------------------------------------------\n");
+  }
+  for (int i = 0; i < count; i++) {
+    printf("[%d-%d]\t%s\trunning\n", processInfoArray[i].startTime,
+           processInfoArray[i].endTime, processtable[i].name);
+  }
+  printf("\nTurnaround Times: ");
+  for (int i = 0; i < count; i++) {
+    printf("%s[%d] ", processtable[i].name, processtable[i].turnaround);
+  }
+  printf("\nWait Times: ");
+  for (int i = 0; i < count; i++) {
+    printf("%s[%d] ", processtable[i].name, processtable[i].wait);
+  }
+  printf("\n\nAverage Turnaround Time: %f", avgTurn);
+  printf("\nAverage Wait Time: %f\n", avgWait);
+}
